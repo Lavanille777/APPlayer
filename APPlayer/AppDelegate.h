6a7115c72e2ed9300425cbdb2b1c6887996ac659
Lavanille777 <lavanille777@gmail.com>
2017-09-27 16:16:23 +0800

    Initial Commit

---
+++ APPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APPlayer
+//
+//  Created by lavanille on 2017/9/27.
+//  Copyright © 2017年 lavanille. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
